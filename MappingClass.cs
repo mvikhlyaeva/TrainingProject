ed151339390bc587a3fd8bae361a7bd82e15d1fb
Мария Вихляева <Mashka.dwe@yandex.ru>
2021-05-14 12:20:00 +0300

    Create MappingClass.cs

---
+++ MappingClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace TrainingProject
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Id { get; set; }
+    }
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Add as many of these lines as you need to map your objects
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
